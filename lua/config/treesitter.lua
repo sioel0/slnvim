1530dcd4baee406903336af1929630b14b256a9e
sioel0 <leonisimone@protonmail.com>
2022-09-13 22:15:03 +0200

    Removed treesitter for latex

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 configs.setup({
-  ensure_installed = {"bash", "c", "cpp", "java", "norg", "python", "latex", "markdown", "yaml", "lua"},
+  ensure_installed = {"bash", "c", "cpp", "java", "norg", "python", "markdown", "yaml", "lua"},
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   auto_install = true,
