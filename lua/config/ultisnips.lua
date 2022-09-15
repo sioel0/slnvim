f91d6e6f98ee74de863051a9bafc6857dd51e451
sioel0 <leonisimone@protonmail.com>
2022-09-15 21:52:48 +0200

    Added some new snippets

--- lua/config/ultisnips.lua
+++ lua/config/ultisnips.lua
@@ -1,6 +1,6 @@
 vim.cmd [[
   let g:UltiSnipsExpandTrigger="<Tab>"
   let g:UltiSnipsJumpForwardTrigger="df"
-  let g:UltiSnipsJumpBackwardTrigger="as"
+  let g:UltiSnipsJumpBackwardTrigger="sd"
   let g:UltiSnipsSnippetDirectories=[$HOME.'/.config/nvim/UltiSnips']
 ]]
