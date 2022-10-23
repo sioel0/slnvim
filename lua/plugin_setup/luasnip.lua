1dc838fb54880df246a314132730b495aaa67b8c
sioel0 <leonisimone@protonmail.com>
2022-10-23 17:19:03 +0200

    Remove couple of shortcuts

--- lua/plugin_setup/luasnip.lua
+++ lua/plugin_setup/luasnip.lua
@@ -5,8 +5,6 @@ local keymap = vim.api.nvim_set_keymap
 
 keymap("i", "<C-j>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
 keymap("i", "<C-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
-keymap("i", "<Tab>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
-keymap("i", "<S-Tab>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
 
 -- setup LuaSnip with vscode-like snippets from friendly-snippets
 require('luasnip.loaders.from_vscode').lazy_load()
